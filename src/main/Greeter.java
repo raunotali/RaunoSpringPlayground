5a401ba9d3146b1174a4e13c3cd8a42e268636a1
Rauno Tali <rauno.tali@cyber.ee>
2016-12-08 16:04:55 +0200

    hello world

---
+++ src/main/Greeter.java
@@ -0,0 +1,10 @@
+package main;
+
+/**
+ * Created by rtali on 8.12.16.
+ */
+public class Greeter {
+    public String sayHello() {
+        return "Hello World!";
+    }
+}
---
+++ src/main/HelloWorld.java
@@ -0,0 +1,11 @@
+package main;
+
+/**
+ * Created by rtali on 8.12.16.
+ */
+public class HelloWorld {
+    public static void main(String[] args) {
+        Greeter greeter = new Greeter();
+        System.out.println(greeter.sayHello());
+    }
+}
