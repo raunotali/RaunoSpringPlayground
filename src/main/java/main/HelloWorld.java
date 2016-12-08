b193c9e30ed3d8f179c382a8155bcbc57b81c6a7
Rauno Tali <rauno.tali@cyber.ee>
2016-12-08 17:58:40 +0200

    Changed Hello World to use Spring framework

---
+++ src/main/java/main/HelloWorld.java
@@ -0,0 +1,32 @@
+package main;
+
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.stereotype.Component;
+
+import com.sun.glass.ui.Application;
+
+@Configuration
+@ComponentScan
+public class HelloWorld {
+
+    @Bean
+    MessageService mockMessageService() {
+        return new MessageService() {
+            public String getMessage() {
+                return "Hello World!";
+            }
+        };
+    }
+
+
+    public static void main(String[] args) {
+        ApplicationContext context =
+            new AnnotationConfigApplicationContext(Application.class);
+        MessagePrinter printer = context.getBean(MessagePrinter.class);
+        printer.printMessage();
+    }
+}
